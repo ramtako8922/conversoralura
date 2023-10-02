f4426c8b2f1870b378c49cc84600af0e538e2a91
Ricardo <rmc892020@gmail.com>
2023-10-02 13:54:26 -0500

    feat  validación de ingreso de datos y cuadros de dialogo de acuerdo a los requerimientos solicitados

--- src/main/java/servicios/Conversor.java
+++ src/main/java/servicios/Conversor.java
@@ -1,121 +1,236 @@
 package servicios;
 
+import ui.InterfazUsuario;
+
 import javax.swing.*;
+import java.net.URISyntaxException;
 
 public class Conversor implements Conversion {
     double valor;
     double resultado;
+    boolean valorValido = false;
+    boolean cierre=false;
+    int opcion;
+
+
 
 
     @Override
     public void convertirPesosDolar() {
-       valor= Double.parseDouble(JOptionPane.showInputDialog("Ingrese el valor en Pesos Colombianos para convertir en dolares"));
-       resultado=(valor*1)/4077.170;
-       JOptionPane.showMessageDialog(null,"El valor en dolares es " +resultado);
+        while (!valorValido) {
+
+                try {
+                    valor = Double.parseDouble(JOptionPane.showInputDialog("Ingrese el valor en Pesos Colombianos para convertir en dolares"));
+                    valorValido = true;
+                    resultado = (valor * 1) / 4077.170;
+
+                    JOptionPane.showMessageDialog(null, "El valor en dolares es " + resultado);
+                    opcion= JOptionPane.showConfirmDialog(null, "¿ Desea continuar en el programa ?  " ,"Advertencia",JOptionPane.YES_NO_CANCEL_OPTION);
+                    if (opcion==JOptionPane.NO_OPTION||opcion==JOptionPane.CANCEL_OPTION){
+                        JOptionPane.showMessageDialog(null, "Programa finalizado ","Gracias",JOptionPane.INFORMATION_MESSAGE);
+                        System.exit(0);
+
+                    }else {
+
+                        InterfazUsuario usuario=new InterfazUsuario();
+                        usuario.ejecutarPrograma();
 
 
-    }
+
+                    }
+
+
+                } catch (NumberFormatException e) {
+                    JOptionPane.showMessageDialog(null, "Solo se aceptan valores numericos", "Error", JOptionPane.ERROR_MESSAGE);
+
+                }
+
+            }
+
+
+
+
+        }
+
+
 
     @Override
     public void convertirDolarPesos() {
+        while (!valorValido) {
+            try {
 
-        valor= Double.parseDouble(JOptionPane.showInputDialog("Ingrese el valor en dolares para convertir en pesos"));
-        resultado=valor*4077.170;
-        JOptionPane.showMessageDialog(null,"El valor en pesos es "+resultado);
+                valor = Double.parseDouble(JOptionPane.showInputDialog("Ingrese el valor en dolares para convertir en pesos"));
+                resultado = valor * 4077.170;
+                JOptionPane.showMessageDialog(null, "El valor en pesos es " + resultado);
 
+            } catch (NumberFormatException e) {
+                JOptionPane.showMessageDialog(null, "Solo se aceptan valores numericos", "Error", JOptionPane.ERROR_MESSAGE);
+
+            }
+        }
     }
 
     @Override
     public void convertirPesosEuro() {
-        valor= Double.parseDouble(JOptionPane.showInputDialog("Ingrese el valor en Pesos Colombianos para convertir en dolares"));
-        resultado=(valor*1)/4311.700;
-        JOptionPane.showMessageDialog(null,"El valor en euros es " +resultado);
+        while (!valorValido) {
+            try {
+                valor = Double.parseDouble(JOptionPane.showInputDialog("Ingrese el valor en Pesos Colombianos para convertir en dolares"));
+                resultado = (valor * 1) / 4311.700;
+                JOptionPane.showMessageDialog(null, "El valor en euros es " + resultado);
 
 
+            } catch (NumberFormatException e) {
+                JOptionPane.showMessageDialog(null, "Solo se aceptan valores numericos", "Error", JOptionPane.ERROR_MESSAGE);
+
+            }
+        }
     }
 
     @Override
     public void convertirEuroPesos() {
 
-        valor= Double.parseDouble(JOptionPane.showInputDialog("Ingrese el valor en euros para convertir en pesos"));
-        resultado=(valor*4077.170)/1;
-        JOptionPane.showMessageDialog(null,"El valor en pesos es "+resultado);
+        while (!valorValido) {
+            try {
+
+                valor = Double.parseDouble(JOptionPane.showInputDialog("Ingrese el valor en euros para convertir en pesos"));
+                resultado = (valor * 4077.170) / 1;
+                JOptionPane.showMessageDialog(null, "El valor en pesos es " + resultado);
 
 
+            } catch (NumberFormatException e) {
+                JOptionPane.showMessageDialog(null, "Solo se aceptan valores numericos", "Error", JOptionPane.ERROR_MESSAGE);
+
+            }
+        }
     }
 
     @Override
     public void convertirPesosLibra() {
-        valor= Double.parseDouble(JOptionPane.showInputDialog("Ingrese el valor en Pesos Colombianos para convertir en libras"));
-        resultado=(valor*1)/4973.900;
-        JOptionPane.showMessageDialog(null,"El valor en euros es " +resultado);
+        while (!valorValido) {
+            try {
+                valor = Double.parseDouble(JOptionPane.showInputDialog("Ingrese el valor en Pesos Colombianos para convertir en libras"));
+                resultado = (valor * 1) / 4973.900;
+                JOptionPane.showMessageDialog(null, "El valor en euros es " + resultado);
 
+            } catch (NumberFormatException e) {
+                JOptionPane.showMessageDialog(null, "Solo se aceptan valores numericos", "Error", JOptionPane.ERROR_MESSAGE);
+            }
+        }
     }
 
     @Override
     public void convertirLibraPesos() {
-        valor= Double.parseDouble(JOptionPane.showInputDialog("Ingrese el valor en libras para convertir en pesos"));
-        resultado=(valor*4973.900)/1;
-        JOptionPane.showMessageDialog(null,"El valor en pesos es "+resultado);
+        while (!valorValido) {
+            try {
+                valor = Double.parseDouble(JOptionPane.showInputDialog("Ingrese el valor en libras para convertir en pesos"));
+                resultado = (valor * 4973.900) / 1;
+                JOptionPane.showMessageDialog(null, "El valor en pesos es " + resultado);
 
+            } catch (NumberFormatException e) {
+                JOptionPane.showMessageDialog(null, "Solo se aceptan valores numericos", "Error", JOptionPane.ERROR_MESSAGE);
+            }
+        }
     }
 
     @Override
     public void convertirPesosYean() {
-        valor= Double.parseDouble(JOptionPane.showInputDialog("Ingrese el valor en Pesos Colombianos para convertir en Yenes"));
-        resultado=(valor*1)/27.278;
-        JOptionPane.showMessageDialog(null,"El valor en yenes es " +resultado);
+        while (!valorValido) {
+            try {
+                valor = Double.parseDouble(JOptionPane.showInputDialog("Ingrese el valor en Pesos Colombianos para convertir en Yenes"));
+                resultado = (valor * 1) / 27.278;
+                JOptionPane.showMessageDialog(null, "El valor en yenes es " + resultado);
 
+            } catch (NumberFormatException e) {
+                JOptionPane.showMessageDialog(null, "Solo se aceptan valores numericos", "Error", JOptionPane.ERROR_MESSAGE);
+            }
+        }
     }
 
     @Override
     public void convertirYeanPesos() {
-        valor= Double.parseDouble(JOptionPane.showInputDialog("Ingrese el valor en yenes para convertir en pesos"));
-        resultado=(valor*27.278)/1;
-        JOptionPane.showMessageDialog(null,"El valor en pesos es "+resultado);
+        while (!valorValido) {
+            try {
+                valor = Double.parseDouble(JOptionPane.showInputDialog("Ingrese el valor en yenes para convertir en pesos"));
+                resultado = (valor * 27.278) / 1;
+                JOptionPane.showMessageDialog(null, "El valor en pesos es " + resultado);
 
+            } catch (NumberFormatException e) {
+                JOptionPane.showMessageDialog(null, "Solo se aceptan valores numericos", "Error", JOptionPane.ERROR_MESSAGE);
+            }
+        }
     }
 
     @Override
     public void convertirPesosWon() {
-        valor= Double.parseDouble(JOptionPane.showInputDialog("Ingrese el valor en Pesos Colombianos para convertir en wom"));
-        resultado=(valor*1)/3.0142387 ;
-        JOptionPane.showMessageDialog(null,"El valor en yenes es " +resultado);
+        while (!valorValido) {
+            try {
+                valor = Double.parseDouble(JOptionPane.showInputDialog("Ingrese el valor en Pesos Colombianos para convertir en wom"));
+                resultado = (valor * 1) / 3.0142387;
+                JOptionPane.showMessageDialog(null, "El valor en yenes es " + resultado);
 
+            } catch (NumberFormatException e) {
+                JOptionPane.showMessageDialog(null, "Solo se aceptan valores numericos", "Error", JOptionPane.ERROR_MESSAGE);
+            }
+        }
     }
 
     @Override
     public void convertirWonPesos() {
-        valor= Double.parseDouble(JOptionPane.showInputDialog("Ingrese el valor en yenes para convertir en pesos"));
-        resultado=(valor*3.0142387 )/1;
-        JOptionPane.showMessageDialog(null,"El valor en pesos es "+resultado);
+        while (!valorValido) {
+            try {
+                valor = Double.parseDouble(JOptionPane.showInputDialog("Ingrese el valor en yenes para convertir en pesos"));
+                resultado = (valor * 3.0142387) / 1;
+                JOptionPane.showMessageDialog(null, "El valor en pesos es " + resultado);
 
+            } catch (NumberFormatException e) {
+                JOptionPane.showMessageDialog(null, "Solo se aceptan valores numericos", "Error", JOptionPane.ERROR_MESSAGE);
+            }
+        }
     }
 
     @Override
     public void convertirCentigradosKelvin() {
+        while (!valorValido) {
+            try {
 
-        valor= Double.parseDouble(JOptionPane.showInputDialog("Ingrese el valor en grados centigrados para convertir en Kelvin"));
-        resultado=(valor+273.15 )/1;
-        JOptionPane.showMessageDialog(null,"El valor en grados Kelvin es "+resultado);
+                valor = Double.parseDouble(JOptionPane.showInputDialog("Ingrese el valor en grados centigrados para convertir en Kelvin"));
+                resultado = (valor + 273.15) / 1;
+                JOptionPane.showMessageDialog(null, "El valor en grados Kelvin es " + resultado);
 
+            } catch (NumberFormatException e) {
+                JOptionPane.showMessageDialog(null, "Solo se aceptan valores numericos", "Error", JOptionPane.ERROR_MESSAGE);
+            }
+        }
     }
 
     @Override
     public void convertirKelvinCentigrados() {
-        valor= Double.parseDouble(JOptionPane.showInputDialog("Ingrese el valor en grados Kelvin para convertir en centigrados"));
-        resultado=(valor-273.15 )/1;
-        JOptionPane.showMessageDialog(null,"El valor en grados centigrados es "+resultado);
+        while (!valorValido) {
+            try {
 
+                valor = Double.parseDouble(JOptionPane.showInputDialog("Ingrese el valor en grados Kelvin para convertir en centigrados"));
+                resultado = (valor - 273.15) / 1;
+                JOptionPane.showMessageDialog(null, "El valor en grados centigrados es " + resultado);
+
+            } catch (NumberFormatException e) {
+                JOptionPane.showMessageDialog(null, "Solo se aceptan valores numericos", "Error", JOptionPane.ERROR_MESSAGE);
+            }
+        }
     }
 
     @Override
     public void convertirCentigradosFarenheiht() {
+        while (!valorValido) {
+            try {
 
-        valor= Double.parseDouble(JOptionPane.showInputDialog("Ingrese el valor en grados centrigrados para convertir en Fahrenheit"));
-        resultado=(valor*1.8)+32;
-        JOptionPane.showMessageDialog(null,"El valor en grados Fahrenheit es "+resultado);
+                valor = Double.parseDouble(JOptionPane.showInputDialog("Ingrese el valor en grados centrigrados para convertir en Fahrenheit"));
+                resultado = (valor * 1.8) + 32;
+                JOptionPane.showMessageDialog(null, "El valor en grados Fahrenheit es " + resultado);
 
 
+            }catch (NumberFormatException e) {
+                JOptionPane.showMessageDialog(null, "Solo se aceptan valores numericos", "Error", JOptionPane.ERROR_MESSAGE);
+            }
+        }
     }
 }
---
+++ src/main/java/ui/InterfazUsuario.java
@@ -0,0 +1,171 @@
+package ui;
+
+import com.sun.tools.javac.Main;
+import servicios.Conversor;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+public class InterfazUsuario {
+
+    //Esta clase renderiza en pantalla todos lo elementos de la interfaz de usuario
+
+    Conversor conversor=new Conversor();
+    public  void ejecutarPrograma() {
+        JFrame frame = new JFrame("CONVERSOR");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(400, 400);
+
+
+        //Creando panel del seleccion del conversor gb
+
+
+        //Agregando elementos al frame
+
+
+        JPanel panel = new JPanel();
+        JMenuBar smb = new JMenuBar();
+        JMenu sm = new JMenu("Seleccione la modena a convertir");
+        JMenuItem sm1 = new JMenuItem("Peso CO Dolar");
+        JMenuItem sm2 = new JMenuItem("Dolar Peso CO");
+        JMenuItem sm3 = new JMenuItem("Peso CO Euro");
+        JMenuItem sm4 = new JMenuItem("Euro Peso CO");
+        JMenuItem sm5 = new JMenuItem("Peso CO Libra Esterlina");
+        JMenuItem sm6 = new JMenuItem("Libra Esterlina Peso CO");
+        JMenuItem sm7 = new JMenuItem("Peso CO Yean");
+        JMenuItem sm8 = new JMenuItem("Yean Peso CO");
+        JMenuItem sm9 = new JMenuItem("Peso CO Yean");
+        JMenuItem sm10 = new JMenuItem("Peso CO Won");
+        JMenuItem sm11 = new JMenuItem(" Won Peso CO");
+        JMenuItem sm12 = new JMenuItem(" Grados °C a grados °K");
+        JMenuItem sm13 = new JMenuItem(" Grados °K a grados °C");
+        JMenuItem sm14 = new JMenuItem(" Grados °C a grados °F");
+        sm.add(sm1);
+        sm.add(sm2);
+        sm.add(sm3);
+        sm.add(sm4);
+        sm.add(sm5);
+        sm.add(sm6);
+        sm.add(sm7);
+        sm.add(sm8);
+        sm.add(sm9);
+        sm.add(sm10);
+        sm.add(sm11);
+        sm.add(sm12);
+        sm.add(sm13);
+        sm.add(sm14);
+        smb.add(sm);
+        panel.add(smb);
+
+
+        sm1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                conversor.convertirPesosDolar();
+
+            }
+        });
+        sm2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                conversor.convertirDolarPesos();
+            }
+        });
+        sm3.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                conversor.convertirPesosEuro();
+            }
+        });
+        sm4.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                conversor.convertirEuroPesos();
+            }
+        });
+
+        sm5.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                conversor.convertirPesosLibra();
+            }
+        });
+        sm6.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                conversor.convertirPesosLibra();
+            }
+        });
+        sm7.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                conversor.convertirLibraPesos();
+            }
+        });
+        sm8.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                conversor.convertirPesosYean();
+            }
+        });
+        sm9.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                conversor.convertirYeanPesos();
+            }
+        });
+        sm10.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                conversor.convertirPesosWon();
+            }
+        });
+        sm11.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                conversor.convertirWonPesos();
+            }
+        });
+
+        sm12.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                conversor.convertirCentigradosKelvin();
+            }
+        });
+
+        sm13.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                conversor.convertirKelvinCentigrados();
+            }
+        });
+
+        sm14.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                conversor.convertirCentigradosFarenheiht();
+            }
+        });
+
+        frame.getContentPane().add(BorderLayout.CENTER, panel);
+        frame.setVisible(true);
+    }
+    public void reiniciarProgrma() throws URISyntaxException {
+        String java=System.getProperty("java.home")+"/bin/java";
+        String jarPath= Main.class.getProtectionDomain().getCodeSource().getLocation().toURI().getPath();
+        String[] comand={java,"-jar",jarPath};
+
+        try {
+        new ProcessBuilder(comand).start();
+            System.exit(0);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
